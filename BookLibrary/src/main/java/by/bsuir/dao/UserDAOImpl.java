6bdaa2b16cf59a30662f1704aa1464adc45ac3d5
yahorzabolotsky <yahorzabolotsky@gmail.com>
2015-12-03 14:24:31 +0300

    implement method 'add' in UserDAOImpl

--- BookLibrary/src/main/java/by/bsuir/dao/UserDAOImpl.java
+++ BookLibrary/src/main/java/by/bsuir/dao/UserDAOImpl.java
@@ -1,11 +1,37 @@
 package by.bsuir.dao;
 
 import by.bsuir.Model.User;
-import by.bsuir.dao.UserDAO;
+import by.bsuir.db.DatabaseManager;
+import by.bsuir.utils.DatabaseUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class UserDAOImpl implements UserDAO {
 
     public void add(User user){
+        try(Connection connection = DatabaseManager.getConnection();
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO Users (id, groupId, login, password) " +
+                            "VALUES(DEFAULT, ?, ?, ?);")){
+            statement.setString(1, user.getGroupId().toString());
+            statement.setString(2, user.getLogin());
+            statement.setString(3, user.getPassword());
+            statement.executeUpdate();
+            Integer insertId = DatabaseUtil.getLastInsertId(connection);
+            if(insertId != null){
+                user.setId(insertId);
+            } else {
+                throw new Exception("Can't insert user.");
+            }
+        } catch (SQLException e) {
+            System.out.println("SQLException in UserDAOImpl.add; FIX OUTPUT IN LOGGER.");
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Exception in UserDAOImpl.add; FIX OUTPUT IN LOGGER.");
+            System.out.println(e.getMessage());
+        }
     }
 
     public void update(User user){
