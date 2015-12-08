ebd87251a27748b1cdff72689e3fa0224114e5b7
yahorzabolotsky <yahorzabolotsky@gmail.com>
2015-12-09 02:14:56 +0300

    configure jdbc driver

--- BookLibrary/src/main/java/by/bsuir/db/DatabaseManager.java
+++ BookLibrary/src/main/java/by/bsuir/db/DatabaseManager.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DatabaseManager {
-    private static final String URL = "jdbc:mariadb://localhost:3306/BookLibrary";
+    private static final String URL = "jdbc:mysql://localhost:3306/BookLibrary";
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "root";
 
